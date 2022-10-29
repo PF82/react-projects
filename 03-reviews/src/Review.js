fd1dabdf34f7608aaa5983c3136223f7e20b9876
PF82 <pedrotcfernandes@gmail.com>
2022-10-29 18:13:40 +0100

    03-reviews

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -3,7 +3,73 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0); // array starts 0
+
+  // get access to a specific object from array
+  const { name, job, image, text } = people[index] // it shows above array index
+  // console.log(people)
+
+  // FaChevronLeft and FaChevronRight buttons
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0 // first array
+    }
+    if (number < 0) {
+      return people.length - 1 // last array
+    }
+    return number;
+  };
+
+  // FaChevronLeft button
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  // FaChevronRight button
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  // Surprise Me button
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1
+    }
+    setIndex(checkNumber(randomNumber))
+    // console.log(randomNumber)
+  }
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={randomPerson}>
+        surprise me
+      </button>
+    </article>
+  )
 };
 
 export default Review;
