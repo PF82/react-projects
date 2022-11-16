e1f546d5aa714396f2f9e45a4e9e71748da7cbc3
PF82 <pedrotcfernandes@gmail.com>
2022-11-16 17:20:11 +0000

    12-sidebar-modal

--- 12-sidebar-modal/src/Sidebar.js
+++ 12-sidebar-modal/src/Sidebar.js
@@ -2,9 +2,51 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+
+  return (
+    // class will be dynamic. hence, the use of template literals for ternary operator
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' :
+      'sidebar'
+      }`}>
+      <div className='sidebar-header'>
+        <img src={logo} className="logo" alt="coding addict" />
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {/* iterate over array of links from data.js */}
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {/* iterate over array of social from data.js */}
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
 }
 
 export default Sidebar
--- 12-sidebar-modal/src/index.js
+++ 12-sidebar-modal/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { AppProvider } from './context'
 import App from './App'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
